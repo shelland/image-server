506919f1f9b97c15e2bbeda12c63b6fd2f87939f
Laserson <laserson@yandex.ru>
2023-02-06 18:57:42 +0700

    Docker fix

--- src/Build.ps1
+++ src/Build.ps1
@@ -1,2 +1,3 @@
 docker build -t shelland/image-server -f Shelland.ImageServer\Dockerfile .
 docker tag shelland/image-server:$CURRENT_TAG$ shelland/image-server:$NEW_TAG$
+docker push shelland/image-server:$NEW_TAG$
