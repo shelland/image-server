89ee4123e7d43427810772cfce0a711fc4f637bb
Laserson <laserson@yandex.ru>
2025-11-13 21:18:38 +0700

    .NET 10

--- src/Shelland.ImageServer/Models/Dto/Request/CreateProcessingProfileRequestDto.cs
+++ src/Shelland.ImageServer/Models/Dto/Request/CreateProcessingProfileRequestDto.cs
@@ -1,13 +1,13 @@
 ﻿// Created on 01/06/2021 19:19 by Andrey Laserson
 
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Shelland.ImageServer.Infrastructure.ModelBinding;
 
 namespace Shelland.ImageServer.Models.Dto.Request;
 
 public record CreateProcessingProfileRequestDto(
-    [Required] string Name,
+    [BindRequired] string Name,
     [ModelBinder(BinderType = typeof(JsonBodyModelBinder))] IReadOnlyCollection<ImageThumbnailParamsDto> Parameters
 );
